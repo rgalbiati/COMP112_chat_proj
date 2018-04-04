0aaa29d520479ec4a6d725f0eadf19c72765651a
Deanna Baris <dbaris01@eecs.tufts.edu>
2018-04-03 23:27:44 -0400

    store city + country based of ip in client

--- clientList.c
+++ clientList.c
@@ -11,7 +11,12 @@
 const static int USER_LEN = 20;
 const static int PASS_LEN = 30;
 const static int SALT_LEN = 16;
+const static int LOC_LEN = 40;
 const static int MAX_CLIENTS = 100;
+
+char *getCountry(char *data);
+char *getCity(char *data);
+char *getGeoData(char *ip);
 
 // generate random string of 16 chars
 void createSalt(char *salt){
@@ -30,7 +35,8 @@ struct clientList *newClientList() {
         memset(c->clients[i]->id, 0, USER_LEN);
         memset(c->clients[i]->pw, 0, PASS_LEN + SALT_LEN);
         memset(c->clients[i]->salt, 0, SALT_LEN);
-        memset(c->clients[i]->country, 0, 40);
+        memset(c->clients[i]->country, 0, LOC_LEN);
+        memset(c->clients[i]->city, 0, LOC_LEN);
         c->clients[i]->fd = -1;
         c->clients[i]->logged_in = false;
         c->clients[i]->numMessages = 0;
@@ -78,8 +84,15 @@ bool addClient(char *id, int fd, char *pw, char *ip, struct clientList *c){
     memset(c->clients[c->numClients]->ip, 0, 16);
     memcpy(c->clients[c->numClients]->ip, ip, strlen(ip) + 1);
 
-    memset(c->clients[c->numClients]->country, 0, 40);
-    // char *country = 
+    char *data = getGeoData(ip);
+    char *country = getCountry(data);
+    char *city = getCity(data);
+    
+    memset(c->clients[c->numClients]->country, 0, LOC_LEN);
+    memcpy(c->clients[c->numClients]->country, country, strlen(country) + 1);
+ 
+    memset(c->clients[c->numClients]->city, 0, LOC_LEN);
+    memcpy(c->clients[c->numClients]->city, city, strlen(city) + 1);
 
     c->clients[c->numClients]->fd = fd;
     c->clients[c->numClients]->logged_in = true;
@@ -88,8 +101,61 @@ bool addClient(char *id, int fd, char *pw, char *ip, struct clientList *c){
     return true;
 }
 
+char *getCountry(char *data){
+    char *tmp = malloc(strlen(data) + 1);
+    memcpy(tmp, data, strlen(data) + 1);
+    int i = 0;
+    char *token = strtok(tmp, ",");
+    while(token){
+        // printf("%s\n", token);
+        if (token[i] == '\"' && token[i + 1] == 'c' && token[i+2] == 'o' &&
+            token[i+3] == 'u' && token[i+4] == 'n' && token[i+5] == 't' && 
+            token[i+6] == 'r' && token[i+7] == 'y' && token[i+8] == '_' && 
+            token[i+9] == 'n' && token[i+10] == 'a' && token[i+11] == 'm' && 
+            token[i+12] == 'e'){
+            char *entry = token;
+            token = strtok(entry, ":");
+            token = strtok(NULL, ",");
 
-char *getCountry(char *ip){
+            // char *city = malloc(strlen(token) + 1);
+            // printf("found country: %s\n", token);
+            char *country = malloc(LOC_LEN);
+            memcpy(country, token, strlen(token) + 1);
+            free(tmp);
+            return country;
+        }
+        token = strtok(NULL, ",");
+    }
+}
+
+char *getCity(char *data){
+    char *tmp = malloc(strlen(data) + 1);
+    memcpy(tmp, data, strlen(data) + 1);
+
+    int i = 0;
+    char *token = strtok(tmp, ",");
+    while(token){
+        // printf("%s\n", token);
+        if (token[i] == '\"' && token[i + 1] == 'c' && token[i+2] == 'i' &&
+            token[i+3] == 't' && token[i+4] == 'y') {
+            char *entry = token;
+            token = strtok(entry, ":");
+            token = strtok(NULL, ",");
+
+            // char *city = malloc(strlen(token) + 1);
+            // printf("found city: %s\n", token);
+            char *city = malloc(LOC_LEN);
+            memcpy(city, token, strlen(token) + 1);
+            free(tmp);
+            return city;
+        }
+        token = strtok(NULL, ",");
+    }
+}
+
+
+char *getGeoData(char *ip){
+    char *url = "freegeoip.net";
     int portno = 80, n, BIG_BUF = 1024;
     struct sockaddr_in serv_addr;
     struct hostent *server;
@@ -97,13 +163,13 @@ char *getCountry(char *ip){
 
     if (sockfd < 0){
         perror("Error opening socket to freegeoip.net\n.");
-        return "";
+        return false;
     }
 
-    server = gethostbyname("ip-api.com");
+    server = gethostbyname("freegeoip.net");
     if (server == NULL){
         perror("No such host.\n");
-        return "";
+        return false;
     }
 
     bzero((char *) &serv_addr, sizeof(serv_addr));
@@ -114,13 +180,13 @@ char *getCountry(char *ip){
     serv_addr.sin_port = htons(portno);
 
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) {
-        perror("Error connecting to ip-api.com\n");
-        return "";
+        perror("Error connecting to freegeoip.net\n");
+        return false;
     }
 
     char *header1 = "GET /json/";
-    char *header2 = " HTTP/1.0\r\nHost: ip-api.com\r\n\r\n";
-    int msg_size = strlen(header1) + strlen(ip) + 1;
+    char *header2 = " HTTP/1.0\r\nHost: freegeoip.net\r\n\r\n";
+    int msg_size = strlen(header1) + strlen(ip) + strlen(header2) + 1;
     char *msg = malloc(msg_size);
     memset(msg, 0, msg_size);
 
@@ -131,22 +197,25 @@ char *getCountry(char *ip){
     n = write(sockfd, msg, strlen(msg));
 
     if (n < 0) {
-        perror("Error writing to ip-api.com\n");
-        return "";
+        perror("Error writing to freegeoip.net\n");
+        return false;
     }
+
+    // printf("post write\n");
 
     char buffer[BIG_BUF];
     memset(buffer, 0, BIG_BUF);
 
     n = read(sockfd, buffer, BIG_BUF);
     if (n < 0) {
-        perror("Error reading from freegeoip.net\n");
-        return "";
+        printf("Error reading from freegeoip.net\n");
+        return false;
     }
-    // printf("BUFFER:\n%s\n",buffer);
+    // printf("BUFFER:\n%s\n", buffer);
 
-    int i = 0, start_i, end_i;
+    int i = 0;
 
+    // GET CITY
     char *token = NULL;
     token = strtok(buffer, "\n\n");
     while(token){
@@ -154,27 +223,12 @@ char *getCountry(char *ip){
         if (token[0] == '{') break;
         token = strtok(NULL, "\n\n");
     }
-    // printf("FINAL TOKEN?\n%s\n", token);
-
-    char *json = token;
-    token = strtok(json, ",");
-    while(token){
-        if (token[i] == '\"' && token[i + 1] == 'c' && token[i+2] == 'i' &&
-            token[i+3] == 't' && token[i+4] == 'y') {
-            break;
-        } 
-        token = strtok(NULL, ",");
-    }
-
-    char *entry = token;
-    token = strtok(entry, ":");
-    token = strtok(NULL, ",");
-
-    char *city = malloc(strlen(token) + 1);
-    strcpy(city, token);
-
+    int len = strlen(token) + 1;
+    char *json = malloc(len);
+    memcpy(json, token, len);
+    return json;
 }
-
+  
 bool hasClient(char *id, struct clientList *c) {
     int numClients = c->numClients;
     for (int i = 0; i < numClients; i++) {
@@ -262,6 +316,16 @@ bool logInClient(char *id, char *pw, char *ip, struct clientList *c) {
                 memcpy(c->clients[i]->ip, ip, strlen(ip) + 1);
 
 
+                char *data = getGeoData(ip);
+                char *country = getCountry(data);
+                char *city = getCity(data);
+                
+                memset(c->clients[c->numClients]->country, 0, LOC_LEN);
+                memcpy(c->clients[c->numClients]->country, country, strlen(country) + 1);
+             
+                memset(c->clients[c->numClients]->city, 0, LOC_LEN);
+                memcpy(c->clients[c->numClients]->city, city, strlen(city) + 1);
+
                 return true;
             }        
         }
@@ -292,7 +356,9 @@ void printClients(struct clientList *c) {
     printf("CLIENT LIST:\n");
     int numClients = c->numClients;
     for (int i = 0; i < numClients; i++) {
-        printf("%d) %s : %d : %s : ", i, c->clients[i]->id, c->clients[i]->fd, c->clients[i]->ip);
+        printf("%d) %s : %d : %s : %s : %s : ", i, 
+                c->clients[i]->id, c->clients[i]->fd, c->clients[i]->ip, 
+                c->clients[i]->city, c->clients[i]->country);
          // printf("%d) %s : %s : %d : ", i, c->clients[i]->id, c->clients[i]->pw, c->clients[i]->fd);
          if (c->clients[i]->logged_in){
             printf("online\n");
@@ -360,6 +426,21 @@ void addPacketMailbox(char *id, struct packet p, struct clientList *c){
     }
 }
 
+// char *cityClientList(char *id, char *city, struct clientList *c){
+//     int numClients = c->numClients, cityMatches = 0;
+//     char *cityClientList = malloc(LOC_LEN * 4);
+//     memset(cityClientList, 0, LOC_LEN * 4);
+//     int bytes = 0;
+
+//     for (int i = 0; i < numClients; i++){
+//         // found a match (not self)
+//         if (strcmp(c->clients[i]->city, city) == 0 && strcmp(c->clients[i]->id, id) != 0){
+            
+//         }
+//     }
+
+//     return cityClientList;
+// }
 
 
 int mailboxSize(struct client *c){
@@ -371,65 +452,82 @@ int mailboxSize(struct client *c){
 // int main() {
 //     printf("RUNNING CLIENTLIST TEST\n");
 //     struct clientList *c = newClientList();
-//     addClient("moi", 1, "secret", c);
-//     addClient("meeee", 2, "password", c);
-//     addClient("deanna", 3, "db", c);
-//     printClients(c);
+//     // char *country = malloc(sizeof(40));
+//     // char *city = malloc(sizeof(40));
+//     // memset(country, 0, 40);
+//     // memset(city, 0, 40);
+//     char *data = getGeoData("208.80.152.201");
+    
+//     char *country = getCountry(data);
+//     printf("Country: %s\n", country);
 
-//     if (!hasClient("moi", c)){
-//         printf("Failed hasClient 'moi'\n");
-//     }
-//     if (hasClient("notInList", c)){
-//         printf("Failed hasClient 'notInList'\n");
-//     }
+//     // printf("%s\n", data);
+//     char *city = getCity(data);
+//     printf("City: %s\n", city);
 
-//     if (getfd("moi", c) != 1 || getfd("meeee", c) != 2 || getfd("deanna", c) != 3){
-//         printf("failed getfd()\n");
-
-//     }
-
-//     if (!isLoggedIn("deanna", c) || (!isLoggedIn("moi", c))){
-//         printf("Failed log in test");
-//         return -1;
-//     }
-
-//     logOutClient("deanna", c);
-
-//     if (isLoggedIn("deanna", c) || !isLoggedIn("moi", c)){
-//         printf("Failed log out test");
-//         return -1;
-
-//     }
-//     printClients(c);
-
-//     logInClient("deanna", "fakenews", c);
-
-//     if (isLoggedIn("deanna", c)){
-//         printf("BAD\n");
-//         return -1;
-//     }
-
-//     logInClient("deanna", "db", c);
-
-//     if (!isLoggedIn("deanna", c)){
-//         printf("should be logged in\n");
-//         return -1;
-//     }
-
-
-//     printClients(c);
-
-
-//     // // remove front
-//     // removeClient("moi", c);
+//     free(city);
+//     free(country);
+//     free(data);
+//     // printf("Country: %s, City: %s\n", country, city);
+//     // addClient("moi", 1, "secret", c);
+//     // addClient("meeee", 2, "password", c);
+//     // addClient("deanna", 3, "db", c);
 //     // printClients(c);
 
-//     // // remove back & add
-//     // removeClient("meeee", c);
+//     // if (!hasClient("moi", c)){
+//     //     printf("Failed hasClient 'moi'\n");
+//     // }
+//     // if (hasClient("notInList", c)){
+//     //     printf("Failed hasClient 'notInList'\n");
+//     // }
+
+//     // if (getfd("moi", c) != 1 || getfd("meeee", c) != 2 || getfd("deanna", c) != 3){
+//     //     printf("failed getfd()\n");
+
+//     // }
+
+//     // if (!isLoggedIn("deanna", c) || (!isLoggedIn("moi", c))){
+//     //     printf("Failed log in test");
+//     //     return -1;
+//     // }
+
+//     // logOutClient("deanna", c);
+
+//     // if (isLoggedIn("deanna", c) || !isLoggedIn("moi", c)){
+//     //     printf("Failed log out test");
+//     //     return -1;
+
+//     // }
+//     // printClients(c);
+
+//     // logInClient("deanna", "fakenews", c);
+
+//     // if (isLoggedIn("deanna", c)){
+//     //     printf("BAD\n");
+//     //     return -1;
+//     // }
+
+//     // logInClient("deanna", "db", c);
+
+//     // if (!isLoggedIn("deanna", c)){
+//     //     printf("should be logged in\n");
+//     //     return -1;
+//     // }
+
+
 //     // printClients(c);
 
 
-//     printf("PASSED TESTS\n");
+//     // // // remove front
+//     // // removeClient("moi", c);
+//     // // printClients(c);
+
+//     // // // remove back & add
+//     // // removeClient("meeee", c);
+//     // // printClients(c);
+
+
+//     // printf("PASSED TESTS\n");
 
 //     freeClientList(c);
 // } 
